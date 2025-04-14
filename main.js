8d6ad98a3c50f841fb1a44014bc89ba6a8ad51d4
TopAnter <topsumikael.maki@gmail.com>
2025-04-14 15:02:58 +0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 document.getElementById("form").addEventListener("submit", formSubmit);
 
 function formSubmit(event){
-    console.log("ada")
+    salasana = document.getElementById("salasana").value
+    document.getElementById("tulostus").innerText =""
+    if(document.getElementById("käytId").value.length < 6){
+        document.getElementById("tulostus").innerText ="Käyttäjä ID liian lyhyt"
+        event.preventDefault()
+        return
+    } else if(salasana.length < 6){
+        let numeroOK = /[0-9]/.test(salasana);
+        let isoKirjainOK = /[A-Z]/.test(salasana);
+        let erikoismerkkiOK = /[!@£$€&%#]/.test(salasana);
+        document.getElementById("tulostus").innerText ="Salasana liian lyhyt"
+        event.preventDefault()
+        return
+    }
     event.preventDefault()
 }
